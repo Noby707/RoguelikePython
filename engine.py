377d939ade60426b22456699b76c65ae009373fb
Ahmed Osman <noby@pop-os.localdomain>
2020-02-24 11:15:41 -0500

    End of part 6

--- engine.py
+++ engine.py
@@ -1,6 +1,8 @@
 import tcod as libtcod
 
-from render_functions import render_all, clear_all
+from components.fighter import Fighter
+from death_functions import kill_monster, kill_player
+from render_functions import render_all, clear_all, RenderOrder
 from entity import Entity, get_blocking_entities_at_location
 from fov_functions import intialize_fov, recompute_fov
 from game_states import GameStates
@@ -30,7 +32,8 @@ def main():
         'light_ground': libtcod.Color(200, 180, 50)
     }
 
-    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True)
+    fighter_component = Fighter(hp=30, defense=2, power=5)
+    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component)
     entities = [player]
 
                                     # FileName,        , GreyScale, Type of file 
@@ -59,7 +62,7 @@ def main():
         if fov_recompute:
             recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
         
-        render_all(con, entities, game_map, fov_map, screen_width, screen_height, colors)
+        render_all(con, entities, player, game_map, fov_map, screen_width, screen_height, colors)
         libtcod.console_flush() # Adds everything to the screen
 
         clear_all(con, entities)
@@ -70,6 +73,8 @@ def main():
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
+        player_turn_results = []
+
         if move and game_state == GameStates.PLAYERS_TURN:
             dx, dy = move
             destination_x = player.x + dx
@@ -79,7 +84,8 @@ def main():
                 target = get_blocking_entities_at_location(entities, destination_x, destination_y)
                 
                 if target:
-                    print('You kick the ' + target.name + ' in the shines, much to its annoyance!')
+                    attack_resutls = player.fighter.attack(target)
+                    player_turn_results.extend(attack_resutls)
                 else:
                     player.move(dx, dy)
 
@@ -92,10 +98,48 @@ def main():
         if fullscreen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
+        for player_turn_result in player_turn_results:
+            message = player_turn_result.get('message')
+            dead_entity = player_turn_result.get('dead')
+
+            if message:
+                print(message)
+            
+            if dead_entity:
+                if dead_entity == player:
+                    message, game_state = kill_player(dead_entity)
+                else:
+                    message = kill_monster(dead_entity)
+                
+                print(message)
+
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
-                if entity != player:
-                    print('The ' + entity.name + ' ponders the meaning of its existence.')
+                if entity.ai:
+                    enemy_turn_results = entity.ai.take_turn(player, fov_map, game_map, entities)
+
+                    for enemy_turn_result in enemy_turn_results:
+                        message = enemy_turn_result.get('message')
+                        dead_entity = enemy_turn_result.get('dead')
+
+                        if message:
+                            print(message)
+            
+                        if dead_entity:
+                            if dead_entity == player:
+                                message, game_state = kill_player(dead_entity)
+                            else:
+                                message = kill_monster(dead_entity)
+                        
+                            print(message)
+
+                            if game_state == GameStates.PLAYERS_DEAD:
+                                break
+                else:
+                    game_state = GameStates.PLAYERS_TURN
+
+        else:
+            game_state = GameStates.PLAYERS_TURN
 
             game_state = GameStates.PLAYERS_TURN
 
